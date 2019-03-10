2540709255a7bf444deac4514e92c86253aa118a
Daniil Kosachev <kos-dan@mail.ru>
2019-03-10 17:17:23 +0300

    Added fucntion for filling matrix

--- Mpi2_LU/Source.cpp
+++ Mpi2_LU/Source.cpp
@@ -6,14 +6,17 @@
 const int N = 4;
 
 void doWork(int myrank, int nprocs, int size);
-inline double getitem(const std::vector<double>& matrix, int i, int j, int n)
+void fillHilbertMatrix(std::vector<double>& matrix, int size);
+
+inline double getitem(const std::vector<double>& matrix, int rows_count, int i, int j)
 {
-    return matrix[i * n + j];
+    return matrix[i * rows_count + j];
 }
-inline void setitem(std::vector<double>& matrix, int i, int j, int n, double value)
+inline void setitem(std::vector<double>& matrix, int rows_count, int i, int j, double value)
 {
-    matrix[i * n + j] = value;
+    matrix[i * rows_count + j] = value;
 }
+
 
 int main(int argc, char* argv[])
 {
@@ -34,26 +37,24 @@ int main(int argc, char* argv[])
 void doWork(int myrank, int nprocs, int size)
 {
     int i, j, k;
-    std::vector<double> a(size * size);
+    int item_count = size * size;
+    std::vector<double> a(item_count);
     if (myrank == 0) {
-        for (i = 0; i < size; i++) {
-            for (j = 0; j < size; j++) {
-                auto value = 1.0 / (i + j + 2 - 1);
-                setitem(a, i, j, size, value);
-            }
-        }
+        fillHilbertMatrix(a, size);
     }
-    MPI_Bcast(static_cast<void*>(a.data()), size * size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    auto raw_void_ptr = static_cast<void*>(a.data());
+    MPI_Bcast(raw_void_ptr, item_count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     std::vector<int> map(size);
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size; i++) {
         map[i] = i % nprocs;
+    }
     for (k = 0; k < size - 1; k++) {
         if (map[k] == myrank) {
-            auto divider = getitem(a, k, k, size);
+            auto divider = getitem(a, size, k, k);
             for (i = k + 1; i < size; i++) {
-                auto old_value = getitem(a, k, i, size);
+                auto old_value = getitem(a, size, k, i);
                 auto new_value = old_value / divider;
-                setitem(a, k, i, size, new_value);
+                setitem(a, size, k, i, new_value);
             }
         }
         auto chunk = a.data() + k * size + k + 1;
@@ -61,11 +62,11 @@ void doWork(int myrank, int nprocs, int size)
         for (i = k + 1; i < size; i++) {
             if (map[i] == myrank) {
                 for (j = k + 1; j < size; j++) {
-                    auto multiplier = getitem(a, i, k, size);
-                    auto k_row_item = getitem(a, k, j, size);
-                    auto old_value = getitem(a, i, j, size);
+                    auto multiplier = getitem(a, size, i, k);
+                    auto k_row_item = getitem(a, size, k, j);
+                    auto old_value = getitem(a, size, i, j);
                     auto new_value = old_value - k_row_item * multiplier;
-                    setitem(a, i, j, size, new_value);
+                    setitem(a, size, i, j, new_value);
                 }
             }
         }
@@ -75,9 +76,19 @@ void doWork(int myrank, int nprocs, int size)
         if (map[i] == myrank) {
             printf("%d:\t", i + 1);
             for (j = 0; j < size; j++)
-                printf("%lg, ", getitem(a, i, j, size));
+                printf("%lg, ", getitem(a, size, i, j));
             printf("\n");
         }
     printf("\n");
     fflush(stdout);
+}
+
+void fillHilbertMatrix(std::vector<double>& matrix, int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            auto value = 1.0 / (i + j + 2 - 1);
+            setitem(matrix, size, i, j, value);
+        }
+    }
 }
